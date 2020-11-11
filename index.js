37f532105889582b0a5d1e5ef8df88f65bba950a
Choharim <jhl8586@naver.com>
2020-11-11 15:40:14 +0900

    Modify answer

--- index.js
+++ index.js
@@ -4,11 +4,13 @@
 const fruits_1 = ['apple', 'banana', 'orange'];
 
 fruits_1.toString();
+fruits_1.join();
 
 //Q2. make an array out of a string
 const fruits_2 = '🍎, 🥝, 🍌, 🍒';
-let array_2 = [];
-array_2.push(fruits_2);
+
+fruits_2.split(",");
+console.log(fruits_2.split(","));
 
 // Q3. make this array look like this: [5, 4, 3, 2, 1]
 const array_3 = [1, 2, 3, 4, 5];
@@ -17,8 +19,9 @@ array_3.reverse();
 
 // Q4. make new array without the first two elements
 const array_4 = [1, 2, 3, 4, 5];
-array_4.shift()
-console.log(array_4);
+
+array_4.slice(0,2)
+console.log(array_4.slice(2));
 
 //
 class Student {
@@ -38,19 +41,11 @@ const students = [
 ];
 // Q5. find a student with the score 90
 
-students.forEach (
-  function(i){
-    if(i.score == 90 ){
-      const name90 = i.name;
-      console.log(name90);
-    } 
-  }
-)
-
 const who90 = students.filter (function(i){ return i.score == 90});
 console.log(who90);
 
 // Q6. make an array of enrolled students
+
 const enrolledStudents = students.filter (function(i){ return i.enrolled === true});
 console.log(enrolledStudents);
 
@@ -64,23 +59,29 @@ students.forEach(
 )
 console.log(scoreArray);
 
+console.log(students.map((i)=> i.score));
+
 // Q8. check if there is a student with the score lower than 50
-
-
-
-// Q9. compute students' average score
-let scoreArray_2 = [];
-students.forEach(
-  function(i){
-    scoreArray_2.push(i.score);
+students.forEach( function(i){
+  if(i.score < 50 ){
+    console.log(true) ;
   }
-);
-
-const sum = scoreArray_2.reduce( function(result,current){
-  return result + current;
 }
 );
-const avg = sum/scoreArray_2.length;
+
+const check = students.some(
+  function(i){
+   return i.score < 50;
+  }
+);
+console.log(check);
+
+// Q9. compute students' average score
+
+const sum = students.reduce( function(result,current){
+  return result + current.score;
+},0);
+const avg = sum/students.length;
 console.log(avg);
 
 // Q10. make a string containing all the scores
@@ -93,7 +94,13 @@ students.forEach(
 const str = scoreArray_3.toString();
 console.log(str);
 
+students.map(function(i){ return i.score;}).toString();
+console.log(students.map(function(i){ return i.score;}).toString());
+
 // Bonus! do Q10 sorted in ascending order
-const bonus = scoreArray_3.sort();
-const strbonus = bonus.toString();
-console.log(strbonus);
+
+const bonus = students.map((i) => i.score).sort((a,b)=> a-b).toString();
+console.log(bonus);
+
+const bonus_2 = students.map((i) => i.score).sort((a,b)=> b-a).toString();
+console.log(bonus_2);
